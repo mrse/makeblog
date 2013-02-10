b00caf0ddddcd3811175c42254d65a3e8a60d1ac
Jake Spurlock <whyisjake@gmail.com>
2013-02-10 14:35:42 -0800

    Changing the order of Javascript elements

--- js/projects.js
+++ js/projects.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function(){
 	jQuery('#tabs div').click(function() {
-		id = jQuery(this).attr('id');
-		googletag.pubads().refresh();
-		_gaq.push(['_trackPageview']);
+		var id = jQuery(this).attr('id');
 		jQuery('.tab-content div#js-' + id).slideDown();
 		jQuery('.tab-content div:not(#js-' + id + ')').slideUp();
+		googletag.pubads().refresh();
+		_gaq.push(['_trackPageview']);
 	});
 });
