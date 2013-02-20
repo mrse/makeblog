04e40fc20598305b0df77822c5bd801fb6106936
Jake Spurlock <whyisjake@gmail.com>
2013-02-20 09:49:27 -0800

    Use the view all link to push a pageview.
    Also optimized images.

--- includes/slideshow.php
+++ includes/slideshow.php
@@ -184,6 +184,9 @@ function make_bs_slideshow() {
 			jQuery(document).ready(function(){
 				jQuery(".viewall").click(function() {
 					jQuery(".carousel-inner").removeClass("carousel-inner");
+					googletag.pubads().refresh();
+					_gaq.push([\'_trackPageview\']);
+					console.log(\'Pushed a pageview, and an ad refresh, like a boss.\');
 				})
 			});
 		</script>
