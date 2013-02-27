3b394e54c7fa229f6a3b4192660d4a022b6f752a
Jake Spurlock <whyisjake@gmail.com>
2013-02-27 09:55:34 -0800

    Fixes for the single page template. Not sure why everything fell apart when I committed, but it sure did...

--- single.php
+++ single.php
@@ -18,69 +18,67 @@ make_get_header() ?>
 				<div class="span12">
 					
 					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		
-						<article <?php post_class(); ?>>
+			
+					<div class="projects-masthead">
 						
-							<div class="projects-masthead">
-								
-								<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-								
-							</div>
-							
-							<ul class="projects-meta">
-								<li>
-									By <?php 
-									if( function_exists( 'coauthors_posts_links' ) ) {	
-										coauthors_posts_links(); 
-									} else { 
-										the_author_posts_link(); 
-									} ?>
-								</li>
-								<li>
-									Category: <?php the_category(', '); ?>
-								</li>
-								<li>
-									<?php the_tags( 'Tags: ', ', '); ?>
-								</li>
-							</ul>
-									
-							<div class="row">
-							
-								<div class="span8">
-
-									<?php the_content(); ?>
-									
-									<?php endwhile; ?>
-
-									<ul class="pager">
-									
-										<li class="previous"><?php previous_posts_link('&larr; Previous Page'); ?></li>
-										<li class="next"><?php next_posts_link('Next Page &rarr;'); ?></li>
-									
-									</ul>
-
-									<?php if (function_exists('make_featured_products')) { make_featured_products(); } ?>
-
-									<div class="comments">
-										<?php comments_template(); ?>
-									</div>
-									
-									<?php else: ?>
-									
-										<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-									
-									<?php endif; ?>
-									
-								</div>
-										
-								<?php get_sidebar(); ?>
-									
-							</div>
-								
-						</article>
-
+						<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+						
 					</div>
-
+					
+					<ul class="projects-meta">
+						<li>
+							By <?php 
+							if( function_exists( 'coauthors_posts_links' ) ) {	
+								coauthors_posts_links(); 
+							} else { 
+								the_author_posts_link(); 
+							} ?>
+						</li>
+						<li>
+							Category: <?php the_category(', '); ?>
+						</li>
+						<li>
+							<?php the_tags( 'Tags: ', ', '); ?>
+						</li>
+					</ul>
+		
 				</div>
+			
+			</div>
+									
+			<div class="row">
+			
+				<div class="span8">
+				
+					<article <?php post_class(); ?>>
 
-		<?php get_footer(); ?>
+						<?php the_content(); ?>
+					
+					</article>
+					
+					<?php endwhile; ?>
+
+					<?php if (function_exists('make_featured_products')) { make_featured_products(); } ?>
+
+					<div class="comments">
+						<?php comments_template(); ?>
+					</div>
+					
+					<?php else: ?>
+					
+						<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+					
+					<?php endif; ?>
+				</div>
+				
+				
+				<?php get_sidebar(); ?>
+					
+					
+			</div>
+
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
