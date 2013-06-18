4cdcef76b21f67b488b5089a853d2921be87f314
Cole Geissinger <cgeissinger@makermedia.com>
2013-06-18 10:26:18 -0700

    Built a dynamic Google Maps page that allows us to pass JSON data via a meta box in the admin area.

--- page-makercamp-map.php
+++ page-makercamp-map.php
@@ -11,10 +11,55 @@
 ?>
 
 <?php get_header( 'makercamp' ); ?>
-	
+	<?php 
+		$get_addresses = get_post_meta( get_the_ID(), 'makercamp-maps-data', false );
+		$addresses = json_decode( str_replace( '&quot;', '"', $get_addresses[0] ), true );
+	?>
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<div class="main-content">
-			<?php the_content(); ?>
+			<div class="container map-wrapper">
+				<div class="row">
+					<div class="span12"><h1 class="map-title text-center">Find A Maker Camp Near You</h1></div>
+					<?php the_content(); ?>
+				</div>
+				<div class="row">
+					<div class="span12">
+						<table class="table table-striped map-list">
+							<thead class="map-list-header">
+								<tr>
+									<th style="width:55px;">Country</th>
+									<th style="width:75px;">State</th>
+									<th style="width:140px;">Program</th>
+									<th style="width:180px;">Organization</th>
+									<th style="width:228px;">Address</th>
+									<th style="width:81px;">City</th>
+									<th style="width:25px;">&nbsp;</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php 
+									if ( ! empty( $addresses ) && is_array( $addresses ) ) :
+										foreach ( $addresses as $address ) : ?>
+											<tr>
+												<td><?php echo esc_attr( $address['Work Country'] ); ?></td>
+												<td><?php echo esc_attr( $address['Work State'] ); ?></td>
+												<td><?php echo esc_attr( $address['Program Name'] ); ?></td>
+												<td>
+													<?php echo esc_attr( $address['Company'] ); ?>
+												</td>
+												<td><?php echo esc_attr( $address['Work Address 1'] ); ?></td>
+												<td><?php echo esc_attr( $address['Work City'] ); ?></td>
+												<td></td>
+											</tr>
+										<?php endforeach;
+									endif;
+								?>
+							</tbody>
+						</table>
+						<?php //echo '<pre>'; var_dump(  $addresses ); echo '</pre>'; ?>
+					</div>
+				</div>
+			</div>
 		</div>
 	<?php endwhile; endif; ?>
 <?php get_footer( 'makercamp' ); ?>
