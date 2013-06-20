ec2e0865b5a33bad96d1a9b35c5013893713a788
Jake Spurlock <whyisjake@gmail.com>
2013-06-20 11:14:11 -0700

    Updates for the road to maker faire page.

--- page-esurance.php
+++ page-esurance.php
@@ -13,20 +13,10 @@ make_get_header() ?>
 		<div class="container">
 
 			<div class="row">
-
-				<div class="span12">
-					
-					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		
-				</div>
-			
-			</div>
-			
-			<div class="row">
 				
 				<div class="span8">
 					
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/RASPBERRYPI_AD.jpg" alt="Raspberry Pi Design Contest">
+					<img src="http://placekitten.com/620/250" alt="Road to Maker Faire Challenge">
 					
 				</div>
 				
@@ -52,52 +42,53 @@ make_get_header() ?>
 				
 				<ul class="nav nav-tabs">
 					<li class="active"><a href="#about" data-toggle="tab">About</a></li>
-					<li class=""><a href="#prizes" data-toggle="tab">Prizes</a></li>
-					<li class=""><a href="#gallery" data-toggle="tab">Gallery of Pi</a></li>
+					<li class=""><a href="#enter" data-toggle="tab">Enter Now</a></li>
+					<li class=""><a href="#entries" data-toggle="tab">View Entries</a></li>
+					<li class=""><a href="#winners" data-toggle="tab">Previous Winners</a></li>
 					<li class=""><a href="#rules" data-toggle="tab">Rules</a></li>
 				</ul>
 				
 				<div class="tab-content">
 					<div class="tab-pane active" id="about">
 						
-						<?php if ( function_exists( 'sharing_display') ) echo sharing_display(); ?> 
+						<?php 
+							if ( have_posts() ) : 
+								while ( have_posts() ) : 
+									the_post();
+									the_content();
+								endwhile;
+							endif;
+						?>
 					
 					</div>
 					
-					<div class="tab-pane" id="prizes">
+					<div class="tab-pane" id="enter">
+						
+						<iframe src = "http://app.wizehive.com/webform/makermedia2013" scrolling="auto" frameborder="0" width="940px" height="2000px"></iframe>
+						<p><a href=”http://www.wizehive.com/photo-contest-software”>Photo Contest Software</a> provided by WizeHive</p>
+						
+					</div>
+					
+					<div class="tab-pane" id="entries">
+					
+						<script src="http://app.wizehive.com/js/portaliframe.js" type="text/javascript"></script><script type="text/javascript">displayPortal('makermedia2013');</script>
+						<p><a href=”http://www.wizehive.com/photo-contest-software”>Photo Contest Software</a> provided by WizeHive</p>
+						
+					</div>
+					
+					<div class="tab-pane" id="winners">
 						
 						
 						
 					</div>
 					
 					<div class="tab-pane" id="rules">
-					
-						
-						
-					</div>
-					
-					<div class="tab-pane" id="gallery">
 						
 						
 						
 					</div>
+					
 				</div>
-			</div>
-									
-			<div class="row">
-			
-				<div class="span12">
-					
-					<?php endwhile; ?>
-
-					<?php else: ?>
-					
-						<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-					
-					<?php endif; ?>
-				
-				</div>
-					
 			</div>
 
 		</div>
