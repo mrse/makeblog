7b20361186f4fd64ea12fc7502519238a5f078f6
Bill Olson <bolson@oreilly.com>
2013-06-21 12:27:05 -0700

    HIW style updates and redirect change for videos page

--- includes/vip.php
+++ includes/vip.php
@@ -148,7 +148,7 @@ if ( function_exists( 'vip_redirects' ) ) {
 		'/faq/index.html'			=> 'http://makezine.com/faq/',
 		'/community/index.html'		=> 'http://archive.makezine.com/community/index.html',
 		'/help/index.html'			=> 'http://archive.makezine.com/help/index.html',
-		'/hardware-innovation-workshop/videos.html', 	'http://archive.makezine.com/hardware-innovation-workshop/videos.html',
+		'/hardware-innovation-workshop/videos.html', 	'http://makezine.com/hardware-innovation-workshop/hardware-innovation-workshop-videos/',
 	);
 	vip_redirects( $redirects );
 }
