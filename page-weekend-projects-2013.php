a44b9722bba66aed9b0577421e4f3d432b31032e
Bill Olson <bolson@oreilly.com>
2013-07-09 14:23:26 -0700

    Correction to maker camp add link that was just committed

--- page-weekend-projects-2013.php
+++ page-weekend-projects-2013.php
@@ -60,7 +60,7 @@ get_header(); ?>
 				<div class="span4">
 					
 					<a href="http://pubads.g.doubleclick.net/gampad/clk?id=40880938&iu=/11548178/Makezine">
-						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/makercamp_300x250.jpg" alt="Maker Camp!" style="margin-bottom:20px;">asdf
+						<img src="<?php echo get_stylesheet_directory_uri(); ?>/img/makercamp_300x250.jpg" alt="Maker Camp!" style="margin-bottom:20px;">
 					</a>
 					
 					<div class="sidebar-ad">
