c37bb783475939f44011b962d8805d2510d3ec6a
Bill Olson <bolson@oreilly.com>
2013-07-12 16:33:36 -0700

    Removing slider from maker training camp home page

--- header-camp.php
+++ header-camp.php
@@ -36,39 +36,6 @@
 	</head>
 	<body <?php body_class('camp'); ?>>
 		<div class="fix">
-			<div id="header">
-                    <div id="slideshow-wrapper">
-                     	<?php if( is_page('trainingcamp') ) { ?> 
-							<div class="container">
-								<div class="row">
-									<div class="span">
-										<div id="myCarousel" class="carousel slide">
-											<!-- Carousel items -->  
-											<div class="carousel-inner">	
-												<div class="item active">
-													<img src="http://makezineblog.files.wordpress.com/2013/05/makertrainingcampbanner2.jpeg?w=940" alt="" />
-													<!-- <div class="carousel-caption">
-															<h4>First Thumbnail label</h4>
-														<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-															</div> -->
-												</div><!-- END item active -->
-												<div class="item"><img src="http://makezineblog.files.wordpress.com/2013/05/makertrainingcampbanner3.jpeg?w=940" alt=""></div>  
-												<div class="item"><img src="http://makezineblog.files.wordpress.com/2013/05/makertrainingcampbanner1.jpg?w=940" alt=""></div>  
-											</div><!--END carousel-inner -->
-											<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
-											<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a> 
-										</div> <!-- END myCarousel carousel slide; -->
-									</div> <!-- END span -->
-								</div> <!-- END row -->
-							</div>	<!-- END container -->
-						<?php } ?>
-					</div> <!-- END slideshow-wrapper -->			
-				<div class="container">	
-							<div class="clear"></div>
-							<h1><a href="http://blog.makezine.com/make-training-camp/"><img style="margin:0 auto" src="http://makezineblog.files.wordpress.com/2013/05/maker-training-camp-hdr.gif" alt="Maker Training Camp" title="Maker Training Camp" /></a></h1>			
-				</div> <!-- END container -->
-			</div> <!-- END header -->
-
 			<div class="header-bottom">
 				<div class="container camp-main-nav">
 					<div class="btn-group nav">
