ebb421a11baf211f36bb138d514e99e56898d780
Jake Spurlock <whyisjake@gmail.com>
2013-07-30 14:47:40 -0700

    Adding some classes. Pulled JS into the tracker.js file.

--- footer.php
+++ footer.php
@@ -144,7 +144,7 @@
 				
 				<div class="span2">
 					
-					<a class="" data-link-name="Subscrive Cover" data-tracker="popup-subscribe-magazine-cover" href="https://readerservices.makezine.com/mk/subscribe.aspx?PC=MK&PK=M37ZPOP">
+					<a class="subscribe-link" data-link-name="Subscribe Cover" data-tracker="popup-subscribe-magazine-cover" href="https://readerservices.makezine.com/mk/subscribe.aspx?PC=MK&PK=M37ZPOP">
 						<img class="cover" src="<?php echo wpcom_vip_get_resized_remote_image_url( 'http://cdn.makezine.com/make/covers/MAKE_V34_high.jpg', 140, 200 ); ?>">
 					</a>
 					
@@ -158,7 +158,7 @@
 				
 				<div class="span2">
 					
-					<a class="button danger" data-link-name="Subscribe Button" data-tracker="popup-subscribe-magazine-button" href="https://readerservices.makezine.com/mk/subscribe.aspx?PC=MK&PK=M37ZPOP">SUBSCRIBE</a>
+					<a class="button danger subscribe-link" data-link-name="Subscribe Button" data-tracker="popup-subscribe-magazine-button" href="https://readerservices.makezine.com/mk/subscribe.aspx?PC=MK&PK=M37ZPOP">SUBSCRIBE</a>
 					<a href="#" data-link-name="Close Button" data-tracker="popup-subscribe-close-button" class="pull-right x"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/x.png" /></a>
 					
 				</div>
@@ -200,10 +200,7 @@
 				
 			});
 			jQuery('.hide-thumbnail').removeClass('thumbnail');
-			jQuery('.popup').delay(5000).fadeIn();
-			jQuery('.x').click(function () {
-				jQuery('.popup').fadeOut();
-    		});
+
 		});
 		</script>
 
