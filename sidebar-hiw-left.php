e28df69560dcd8c30cd63d46f43d8b5d36d61b23
Bill Olson <bolson@oreilly.com>
2013-08-07 11:53:38 -0700

    Updated side navigation for HIW

--- sidebar-hiw-left.php
+++ sidebar-hiw-left.php
@@ -13,8 +13,10 @@
 						<li><a href="<?php echo home_url( '/hardware-innovation-workshop/' ); ?>">Home</a></li>
 						<li><a href="<?php echo home_url( 'hardware-innovation-workshop/2013-new-york-event/' ); ?>">2013 New York Event</a></li>
 						<div style="padding-left:15px;">
-								<li><a href="<?php echo home_url( 'hardware-innovation-workshop/2013-new-york-event/tickets/' ); ?>">Tickets</a></li>
-                        	</div>  
+							<li><a href="<?php echo home_url( 'hardware-innovation-workshop/2013-new-york-event/agenda/' ); ?>">Agenda</a></li>
+							<li><a href="<?php echo home_url( 'hardware-innovation-workshop/2013-new-york-event/speakers/' ); ?>">Speakers</a></li>
+							<li><a href="<?php echo home_url( 'hardware-innovation-workshop/2013-new-york-event/tickets/' ); ?>">Tickets</a></li>
+                    	</div>  
 						<li><a href="<?php echo home_url( '/hardware-innovation-workshop/research/' ); ?>">Maker Market Research</a></li>
 						<li><a href="<?php echo home_url( '/hardware-innovation-workshop/press/' ); ?>">Press</a></li>
 						<li><a href="<?php echo home_url( '/hardware-innovation-workshop/contact/' ); ?>">Contact</a></li>
