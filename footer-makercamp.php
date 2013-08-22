300b1413eaaaf3f4bd6742471ea43213f4ff58fd
Bill Olson <bolson@makermedia.com>
2013-08-22 15:14:06 -0700

    Removing FAQ from footer

--- footer-makercamp.php
+++ footer-makercamp.php
@@ -83,7 +83,7 @@
 								<div class="about-column-02">
 									<ul>
 										<li><a href="http://makermedia.com/about-us/management-team/" target="_blank">About Us</a></li>
-										<li><a href="http://makezine.com/faq/index.html" target="_blank">FAQ</a></li>
+										<!-- <li><a href="http://makezine.com/faq/index.html" target="_blank">FAQ</a></li> -->
 										<li><a href="http://makezine.com/community/index.html" target="_blank">Forums</a></li>
 										<li><a href="http://makezine.com/contribute/" target="_blank">Write for MAKE</a></li>
 									</ul>
