8e65140b6590a95d015ecf01320b51b4dacd2314
Cole Geissinger <cgeissinger@makermedia.com>
2013-09-15 09:00:41 -0700

    Updating the Maker Faire slider content

--- page-mf-featured.php
+++ page-mf-featured.php
@@ -3,10 +3,13 @@
  * @package MakeZine
  * Template Name: MF Featured Posts
  */
+?>
+
+<?php
 	$args = array(
 		'post_type' => array( 'post', 'craft', 'magazine', 'video', 'projects' ),
 		'post_status' => 'publish',
-		'posts_per_page' => 5,
+		'posts_per_page' => 7,
 		'ignore_sticky_posts' => 1,
 		'tag' => 'mf-featured'
 	);
@@ -16,16 +19,16 @@
 	if ( $query->have_posts() ) : ?>
 		<div id="mf-featured-slider" class="carousel slide">
 			<div class="carousel-inner">
-				<?php while ( $query->have_posts() ) : $query->the_post(); 
+				<?php $i = 1; while ( $query->have_posts() ) : $query->the_post(); 
 					$image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full'); ?>
-					<div class="item active">
-						<a href="<?php get_permalink(); ?>"><img src="<?php echo wpcom_vip_get_resized_remote_image_url( $image, 366, 342 ); ?>" alt="..."></a>
+					<div class="item<?php echo ( $i % 5 == 1 ) ? ' active' : ''; ?>">
+						<a href="<?php get_permalink(); ?>"><img src="<?php echo wpcom_vip_get_resized_remote_image_url( $image[0], 366, 342 ); ?>"></a>
 						<div class="carousel-caption">
 							<h3 class="cap-title">Maker Faire News</h3>
-							<h4 class="cap-body"><a href="<?php get_peramlink(); ?>"><?php the_title(); ?></a></h4>
+							<h4 class="cap-body"><a href="<?php get_permalink(); ?>"><?php the_title(); ?></a></h4>
 						</div>
 					</div>
-				<?php endwhile; ?>
+				<?php $i++; endwhile; ?>
 			</div>
 			<?php if ( $query->found_posts >= 2 ) : ?>
 				<a class="left carousel-control" href="#mf-featured-slider" data-slide="prev"><span class="icon-prev"><</span></a>
