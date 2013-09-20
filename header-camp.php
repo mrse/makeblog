294415d5c0e32dc51f9e9a0b8411276647b8b69e
David Beauchamp <dbeauchamp@makermedia.com>
2013-09-20 16:45:05 -0700

    Added a new function for generating the title tag

--- header-camp.php
+++ header-camp.php
@@ -2,7 +2,7 @@
 <html lang="en" xmlns:fb="http://ogp.me/ns/fb#">
 	<head>
 		<meta charset="utf-8">
-		<title><?php bloginfo('name'); ?> | <?php is_home() ? bloginfo('description') : wp_title(''); ?></title>
+		<title><?php echo make_genenate_title_tag(); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 
