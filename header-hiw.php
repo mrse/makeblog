2f157e7d53b110a0c6ea7c1361f1271123713e78
Cole Geissinger <cgeissinger@makermedia.com>
2013-09-26 12:59:58 -0700

    one more to fix

--- header-hiw.php
+++ header-hiw.php
@@ -2,7 +2,7 @@
 <html lang="en" xmlns:fb="http://ogp.me/ns/fb#">
 	<head>
 		<meta charset="utf-8">
-		<title><?php echo make_genenate_title_tag(); ?></title>
+		<title><?php echo make_generate_title_tag(); ?></title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="description" content="">
 
