7f909ff676a4b1683eb5c2c9c0b15029a4239d0a
Jake Spurlock <whyisjake@gmail.com>
2013-10-08 15:10:15 -0700

    Updated index.php

--- index.php
+++ index.php
@@ -1,66 +1,87 @@
-<?php get_header(); ?>
-
-		<div class="single">
+<?php
+/**
+ * Index Page
+ *
+ * @package    makeblog
+ * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
+ * @author     Jake Spurlock <jspurlock@makermedia.com>
+ * 
+ */
+make_get_header() ?>
 		
-			<div class="container">
+	<div class="single">
+	
+		<div class="container">
 
-				<div class="row">
+			<div class="row">
 
-					<div class="span8">
-
-						<?php if (!is_home()) { ?>
-
-							<ul class="breadcrumb">
+				<div class="span12">
+					
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			
-								<?php if(class_exists('bcn_breadcrumb_trail')) {
-									$breadcrumb_trail = new bcn_breadcrumb_trail;
-									$breadcrumb_trail->opt['home_title'] = "Home";
-									$breadcrumb_trail->opt['current_item_prefix'] = '<li class="current">';
-									$breadcrumb_trail->opt['current_item_suffix'] = '</li>';
-									$breadcrumb_trail->opt['separator'] = '<span class="divider">&nbsp;/&nbsp;</span>';
-									$breadcrumb_trail->opt['home_prefix'] = '<li>';
-									$breadcrumb_trail->opt['home_suffix'] = '</li>';
-									$breadcrumb_trail->opt['max_title_length'] = 70;
-									$breadcrumb_trail->fill();
-									$breadcrumb_trail->display();
-								} ?>
-										
-							</ul>
-
-						<?php } ?>
-
-						<div class="content" id="content">
-
-						<?php
-							if ( have_posts() ) :
-								while ( have_posts() ) :
-									the_post();
-									get_template_part( 'content' );
-								endwhile; ?>
-
-								<ul class="pager">
-							
-									<li class="previous"><?php previous_posts_link('&larr; Previous Page'); ?></li>
-									<li class="next"><?php next_posts_link('Next Page &rarr;'); ?></li>
-								
-								</ul>
-							
-							<?php else: ?>
-							
-								<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
-							
-							<?php endif; ?>
-
-						</div>
-
-						<div class="content">
-
-							<?php if (function_exists('make_featured_products')) { make_featured_products(); } ?>
-
-						</div>
-
+					<div class="projects-masthead">
+						
+						<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+						
 					</div>
+					
+					<ul class="projects-meta">
+						<li>
+							By <?php 
+							if( function_exists( 'coauthors_posts_links' ) ) {	
+								coauthors_posts_links(); 
+							} else { 
+								the_author_posts_link(); 
+							} ?>
+						</li>
+						<li>
+							Posted <span class="blue"><?php the_date('m/d/Y \@ g:i a'); ?></span>
+						</li>
+						<li>
+							Category <?php the_category(', '); ?>
+						</li>
+						<li>
+							Comments <a href="<?php the_permalink(); ?>#comments"><?php comments_number( '0', '1', '%' ); ?></a>
+						</li>
+					</ul>
+		
+				</div>
+			
+			</div>
+									
+			<div class="row">
+			
+				<div class="span8">
+				
+					<article <?php post_class(); ?>>
 
-					<?php get_sidebar(); ?>
+						<?php the_content(); ?>
+					
+					</article>
+					
+					<?php endwhile; ?>
 
-			<?php get_footer(); ?>
+					<?php if (function_exists('make_featured_products')) { make_featured_products(); } ?>
+
+					<div class="comments">
+						<?php comments_template(); ?>
+					</div>
+					
+					<?php else: ?>
+					
+						<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+					
+					<?php endif; ?>
+				</div>
+				
+				
+				<?php get_sidebar(); ?>
+					
+					
+			</div>
+
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
