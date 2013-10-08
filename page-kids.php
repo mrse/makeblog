480ef42b5b376c94efe75fc919ccf868aa89e99c
Jake Spurlock <whyisjake@gmail.com>
2013-10-08 11:15:43 -0700

    Start of the new kids page. This isn't done yet...

--- page-kids.php
+++ page-kids.php
@@ -1,9 +1,89 @@
 <?php
 /**
+ *
  * Template Name: Kids
+ * @package    makeblog
+ * @license    http://opensource.org/licenses/gpl-license.php  GNU Public License
+ *
  */
-?>
+make_get_header() ?>
+		
+	<div class="single">
+	
+		<div class="container">
 
+			<div class="row">
+
+				<div class="span12">
+					
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			
+					<div class="projects-masthead">
+						
+						<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+						
+					</div>
+					
+					<ul class="projects-meta">
+						<li>
+							By <?php 
+							if( function_exists( 'coauthors_posts_links' ) ) {	
+								coauthors_posts_links(); 
+							} else { 
+								the_author_posts_link(); 
+							} ?>
+						</li>
+						<li>
+							Posted <span class="blue"><?php the_date('m/d/Y \@ g:i a'); ?></span>
+						</li>
+						<li>
+							Category <?php the_category(', '); ?>
+						</li>
+						<li>
+							Comments <a href="<?php the_permalink(); ?>#comments"><?php comments_number( '0', '1', '%' ); ?></a>
+						</li>
+					</ul>
+		
+				</div>
+			
+			</div>
+									
+			<div class="row">
+			
+				<div class="span8">
+				
+					<article <?php post_class(); ?>>
+
+						<?php the_content(); ?>
+					
+					</article>
+					
+					<?php endwhile; ?>
+
+					<?php if (function_exists('make_featured_products')) { make_featured_products(); } ?>
+
+					<div class="comments">
+						<?php comments_template(); ?>
+					</div>
+					
+					<?php else: ?>
+					
+						<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+					
+					<?php endif; ?>
+				</div>
+				
+				
+				<?php get_sidebar(); ?>
+					
+					
+			</div>
+
+		</div>
+
+	</div>
+
+<?php get_footer(); ?>
 
 <?php get_header(); ?>
 		
