641aca9b7ae9a9a36cfadc5a484b4ba844a06e3d
Bill Olson <bolson@makermedia.com>
2013-10-18 17:19:18 -0700

    Spelling error in electronics category ad

--- dfp.php
+++ dfp.php
@@ -121,7 +121,7 @@ global $post;
 				elseif (is_single(array(122575))) {
 					echo "googletag.pubads().setTargeting('sponsor',['xobject']);";
 				}
-				elseif ( is_category( array( 'electronics', 'arduino', 'computers-mobile', 'raspberry-pi', 'robotics' ) ) || post_is_in_descendant_category( 'elctronics' ) || has_category( 'electronics' ) ) {
+				elseif ( is_category( array( 'electronics', 'arduino', 'computers-mobile', 'raspberry-pi', 'robotics' ) ) || post_is_in_descendant_category( 'electronics' ) || has_category( 'electronics' ) ) {
 					echo "googletag.pubads().setTargeting('sponsor',['element14']);";
 				}
 				elseif (is_page( array( 289746,271575 ) ) ) {
