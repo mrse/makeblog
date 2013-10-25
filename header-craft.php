23800f3983d9daea07d07e8723f6b5dc30bc3102
Jake Spurlock <whyisjake@gmail.com>
2013-10-25 15:37:50 -0700

    Adding esc_attr() to the function to generate the description tag. This came from this ticket: https://wordpressvip.zendesk.com/requests/22519

--- header-craft.php
+++ header-craft.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title><?php echo make_generate_title_tag(); ?></title>
-		<meta name="description" content="<?php echo make_generate_description(); ?>" />
+		<meta name="description" content="<?php echo esc_attr( make_generate_description() ); ?>" />
 
 		<meta name="p:domain_verify" content="c4e1096cb904ca6df87a2bb867715669" >
 		<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
