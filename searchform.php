925fbc7ce7e0650f286b5aedf192f2b70ee1187d
Jake Spurlock <whyisjake@gmail.com>
2013-11-01 15:54:15 -0700

    Bringing in Elastic Search

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form role="search" method="get" class="form-search" id="searchform" action="<?php echo home_url( '/' ); ?>">
+	<input type="text" class="input-medium search-query" value="" name="s" id="s" placeholder="Search" />
+	<input type="submit" class="btn" id="searchsubmit" value="Search" />
+</form>
